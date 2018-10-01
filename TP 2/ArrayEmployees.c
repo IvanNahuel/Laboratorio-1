7bbc2b59c37a24722166cbc25810ea986c05c081
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2018-10-01 19:28:41 -0300

    Add files via upload

--- TP 2/ArrayEmployees.c
+++ TP 2/ArrayEmployees.c
@@ -14,7 +14,7 @@ typedef struct
 int initEmployees(eEmployee* list,int len){
     int retorno=-1;
     if (list!=NULL && len>0){
-        for (int i=0;i<cant;i++){
+        for (int i=0;i<len;i++){
             list[i].isEmpty=1;
             retorno=0;
         }
@@ -27,13 +27,50 @@ int initEmployees(eEmployee* list,int len){
 *
 */
 
-int addEmployee(Employee* list, int len, int id, char name[],char lastName[],float salary,int sector){
-
-
-
-
+int addEmployee(eEmployee* list, int len, int id, char name[],char lastName[],float salary,int sector){
+    int retorno=-1;
+    if (list!=NULL && len>0){
+        strcpy (list[id].name,name);
+        strcpy (list[id].lastName,lastName);
+        list[id].salary = salary;
+        list[id].sector = sector;
+        list[id].isEmpty=0;
+        retorno=0;
+    }
  return -1;
 }
+int findEmployeeById(eEmployee* list, int len,int id){
+    int retorno=-1;
+    if (list!=NULL && len>0){
+        for (int i=0;i<len;i++){
+            if (list[i].id==id){
+                retorno=i;
+                break;
+            }
+        }
+    }
+ return retorno;
+}
+
+int removeEmployee(eEmployee* list, int len, int id){
+    int retorno=-1;
+    if (list!=NULL && len>0){
+        for (int i=0;i<len;i++){
+            if (list[i].id==id){
+                list[i].isEmpty=1;
+                retorno=0;
+                break;
+            }else {
+            retorno=-1;
+            }
+        }
+    }
+ return retorno;
+}
+
+
+
+
 
 
 
