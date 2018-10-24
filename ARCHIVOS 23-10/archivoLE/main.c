96211973d941d532b898e7789d2282bc0dcefced
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2018-10-23 22:03:54 -0300

    Add files via upload

---
+++ ARCHIVOS 23-10/archivoLE/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+#define ARCH	".\\bin.dat"
+#define ESC 27
+
+struct a{
+		char nombre[31];
+		int edad;
+	};
+
+int main (void)
+{
+
+
+    int respuesta=1;
+    FILE  *bin;
+    struct a pers;
+    int cant;
+
+   if ((bin=fopen(ARCH,"rb+"))==NULL){
+      if ((bin=fopen(ARCH,"wb+"))==NULL){
+         printf("No se pudo abrir el archivo");
+         exit(1);
+      }
+   }
+
+
+   do{
+      printf("\nIngrese el nombre: ");
+      gets(pers.nombre);
+
+      printf("Ingrese la edad: ");
+      scanf("%d",&pers.edad);
+
+      fflush(stdin);
+      fseek(bin , 0L, SEEK_END);
+      fwrite(&pers,sizeof(pers),1,bin);
+
+      printf("\nPresione ESC para terminar");
+   }while((getche())!=ESC);
+
+   //se lleva el indicador de posición al principio para comenzar a leer
+
+   rewind (bin);
+
+   while(!feof(bin)){
+      cant = fread(&pers,sizeof(pers),1,bin);
+
+      if(cant!=1){
+         if(feof(bin)){
+			break;
+         }
+         else{
+			printf("No leyo el ultimo registro");
+			break;
+         }
+      }
+
+      printf("\n%s\t%d",pers.nombre,pers.edad);
+      //aca debo comparar si es igual a la edad ingresada, si lo es
+      //debo retroceder una posicion y modificar lo que debo modificiar
+
+
+   }
+
+   fclose(bin);
+   getch();
+
+   return 0;
+}
+
+void ImprimirMenu(){
+printf("\n1-Altas");
+printf("\n2-Listado");
+
+}
+
+
