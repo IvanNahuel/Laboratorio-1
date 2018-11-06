2378b141a5b8cfde02e6ebf477f1f8a8ff6e5c0b
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2018-11-05 21:32:03 -0300

    Add files via upload

---
+++ TP3/Win_64/Employee.c
@@ -0,0 +1,50 @@
+int employee_setId(Employee* this,int id){
+    if (this!=NULL){
+        if(id > 0){
+            this->id = id;
+        }
+    }
+}
+int employee_getId(Employee* this,int* id){
+    if (this!=NULL){
+        return this->id;
+    }
+}
+
+int employee_setNombre(Employee* this,char* nombre){
+    if (this!=NULL){                    //si el puntero a persona es distinto a nulo
+        strcpy(this->nombre, nombre);   //en la posicion pasada por parametro copiame
+    }
+}
+int employee_getNombre(Employee* this,char* nombre){
+
+
+
+}
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas){
+    if (this!=NULL){
+        if(horasTrabajadas > 0){
+            this->horasTrabajadas = horasTrabajadas;
+        }
+    }
+}
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas){
+    if (this!=NULL){
+        return this->horasTrabajadas;
+    }
+}
+int employee_setSueldo(Employee* this,int sueldo){
+    if (this!=NULL){
+        if(sueldo > 0){
+            this->sueldo = sueldo;
+        }
+    }
+}
+int employee_getSueldo(Employee* this,int* sueldo){
+    if (this!=NULL){
+        return this->sueldo;
+    }
+}
+
+
+
